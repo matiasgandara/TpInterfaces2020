c76e4ae6d1563b2b3f045e23c05f47448b6cd47d
matiasgandara <gandaramatias@gmail.com>
2020-10-20 22:37:04 -0300

    version final

---
+++ TP3/IntegradorTP3/js/scrollybat.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+'use strict'
+
+let pagina = document.querySelector("#pagina");
+pagina.classList.toggle("oculto");
+
+let spinner = document.querySelector(".loading");
+setTimeout(function(){ 
+    
+    spinner.classList.toggle("oculto"); 
+    pagina.classList.toggle("oculto")}, 3000);
+
+
+let bg = document.querySelector("#cielo");
+let logo = document.querySelector("#logo");
+let nubes = document.querySelector("#nubes");
+let road = document.querySelector("#road");
+let text = document.querySelector("#text");
+let murcielago= document.querySelector("#murcielago");
+let card1 = document.querySelector("#card1");
+let card2 = document.querySelector("#card2");
+
+
+//***scrollY para parallax inicial */
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+    bg.style.top = value * 0.5 + 'px';
+    logo.style.top = value * 0.9 + 'px';
+    logo.style.left = value * 0.05 + 'px';
+    murcielago.style.top = -value * 0.8 + 'px'; 
+    nubes.style.top = -value * 0.5 + 'px'; 
+    road.style.top = value * 0.15 + 'px';
+    text.style.top = value * 1 + 'px';
+    text.style.left = value * 0.2 + 'px';
+});
+
+
+
+
+});
